976f4d9efe6f7badeb7e940343a4217813762f8c
steamruler <steamruler@gmail.com>
2012-09-21 18:15:27 +0200

    Initial Developement Push.
    Added basic code, no testing has occured.

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 CloudServer - Open Source HTML Server
 ===========
-Copyright 2012, Sebastian Johansson
+Licence
 -----------
+Copyright (c) 2012 Sebastian Johansson
 
-You are allowed to create and adapt this project for private, non-commercial use. I may change this at any point I want.
+Permission is hereby granted, free of charge, to any pony obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit ponies to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 Stats
 -----------
@@ -21,34 +26,40 @@ Requires
 
 * Ethernet Shield
 
-* MicroSD
+* FAT16/32 Formatted MicroSD
 
 Repo Info
 ===========
-/src/				Source for the software
+Folder specifications
+-----------
+/src/				- Source for the software
 
-/assets_src/		Source for the assets
+/assets_src/		- Source for the assets
 
-/assembly/			Assembly instructions
+/assembly/			- Assembly instructions
 
-/microsd/			MicroSD Files
+/microsd/			- MicroSD Files
 
-/misc/				Stuff that does not fit anywhere else
+/misc/				- Stuff that does not fit anywhere else
 
-/misc/references/	References, archived
+/misc/references/	- References, archived
 
 Functions
 ===========
 Primary (and Important)
 -----------
 * Web Panel on alternative port capable of reading logs, easy editing of settings and uploading files
-
 * INI table with content-type and file extention pairings
-
-* INI table with settings such as disabling Directory Listings etc.
 
 Secondary
 -----------
+
+
+Finished
+-----------
 * DCHP Configuration
 
-* FTP Server
+Aborted
+-----------
+* FTP Server (Reason: Not enough memory on the Arduino to efficiently handle it.)
+* INI table with settings such as disabling Directory Listings etc. (Reason: Uses separate files instead)
---
+++ src/CloudServer/CloudServer.ino
@@ -0,0 +1,126 @@
+//Is debugging enabled?
+#define __d3bug true
+
+//What is the SDCard Pin? Standard for Ethernet Shield is 4.
+#define __sdcardpin 4
+//What is the IP? Splitted in 4 parts!
+#define _0_ip 192
+#define _1_ip 168
+#define _2_ip 1
+#define _3_ip 177
+
+//Includes
+#include <SPI.h> 
+#include <SD.h>
+#include <Dhcp.h>
+#include <Dns.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <util.h>
+#include <stdlib.h>
+
+//Variables
+EthernetServer webserver_main = EthernetServer(atoi(ReadFile("/system/settings/PORT")));
+EthernetServer webserver_cpanel = EthernetServer(atoi(ReadFile("/system/settings/CPPORT")));
+
+void setup()
+{
+  setupSD();
+  if (__d3bug)
+  {
+    Serial.begin(9600);
+  }
+}
+
+void loop()
+{
+  EthernetClient pone = webserver_main.available();
+  EthernetClient admin_pone = webserver_main.available();
+  if (pone)
+  {
+    boolean currentLineIsBlank = true;
+    while (pone.connected()) {
+      if (pone.available()) {
+        char c = pone.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) {
+          // send a standard http response header
+          pone.println("HTTP/1.1 200 OK");
+          pone.println("Content-Type: text/html");
+          pone.println("Connnection: close");
+          pone.println();
+          pone.println(ReadFile("/htdocs/index.htm"));
+          break;
+        }
+        if (c == '\n') {
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    delay(1);
+    pone.stop();
+  }
+  if (admin_pone)
+  {
+    boolean currentLineIsBlank = true;
+    while (admin_pone.connected()) 
+    {
+      if (admin_pone.available()) 
+      {
+        char c = admin_pone.read();
+        Serial.write(c);
+        if (c == '\n' && currentLineIsBlank) 
+        {
+          // send a standard http response header
+          pone.println("HTTP/1.1 200 OK");
+          pone.println("Content-Type: text/html");
+          pone.println("Connnection: close");
+          pone.println();
+          pone.println(ReadFile("/system/webpanel/index.htm"));
+          break;
+        }
+        if (c == '\n') 
+        {
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') 
+        {
+          currentLineIsBlank = false;
+        }
+      }
+    }
+  }
+}
+
+void setupSD()
+{
+  pinMode(10, OUTPUT);
+  SD.begin(__sdcardpin);
+}
+
+char* ReadFile(char cFile[])
+{
+  File FileToBuckingRead = SD.open(cFile);
+  int numberoftexts = 0;
+  char Buffer[33];
+  
+  if (!FileToBuckingRead)
+  {
+    return "404";
+  }
+  while (FileToBuckingRead.available())
+  {
+    char curcharacter = FileToBuckingRead.read();
+    numberoftexts++;
+    Buffer[numberoftexts - 1] = curcharacter;
+  }
+  Buffer[numberoftexts] = '\0';
+  FileToBuckingRead.close();
+  return Buffer;
+}
+
